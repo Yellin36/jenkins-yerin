af1da1e4838cb972db7e0b8f70c55f64e593b694
yerin <yerin@yerinui-MacBookAir.local>
2023-01-02 15:55:08 +0900

    first

---
+++ src/main/java/com/example/jenkinsyerin/JenkinsYerinApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsyerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsYerinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsYerinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsyerin/JenkinsYerinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsyerin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsYerinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
